63df51e412ba47763cc27522c7f3620c71733f77
radu <radu.zevri.ichc@gmail.com>
2016-08-22 21:07:21 +0200

    Sorting Algorithms

---
+++ sorting_algo/bubble-sort.js
@@ -0,0 +1,19 @@
+
+
+function Sort(array)
+{
+	for (var i=0;i<array.length-1;i++)
+	{
+		for (var j=i+1;j<array.length;j++) 
+		{
+			if(array[i]>array[j])
+			{
+				var temp=array[i];
+				array[i]=array[j];
+				array[j]=temp;
+			}
+		}
+	}
+	document.getElementById("sorted").innerHTML=array.toString();
+}
+
